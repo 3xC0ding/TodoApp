dd585d49af325af8b9bb08aeffdf7ac98f9e7f34
leoneldosanjos01 <leoneldosanjos01@gmail.com>
2020-11-12 13:55:25 +0100

    Backend setup

---
+++ backend/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
